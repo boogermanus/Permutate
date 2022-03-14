65cb0ad3725419c977b26f3f5302f8d701166a9c
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-03-14 18:57:42 -0500

    initial commit

---
+++ Permutate.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PermutateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Permutate/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Permutate
+{
+    public class Class1
+    {
+    }
+}
